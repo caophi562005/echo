778440be99a5161bc86aa9a1420843f00fce3be0
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2026-02-05 21:41:28 +0700

    Convex Package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,15 @@
-import { add } from "@workspace/math/add";
-import { Button } from "@workspace/ui/components/button";
+"use client";
+
+import { api } from "@workspace/backend/_generated/api";
+import { useQuery } from "convex/react";
 
 export default function Page() {
+  const users = useQuery(api.user.getMany);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <div className="flex gap-2">
-          <Button>Button</Button>
-          <Button variant="outline">Outline</Button>
-          <p>{add(1, 2)}</p>
-        </div>
-      </div>
+      <p>apps/web</p>
+      {JSON.stringify(users)}
     </div>
   );
 }
