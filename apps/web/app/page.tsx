d6676ef3493053055dc7fa9be179115cbbe352ff
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2026-02-06 00:58:44 +0700

    Clerk Authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import { api } from "@workspace/backend/_generated/api";
-import { useQuery } from "convex/react";
+import { Authenticated, Unauthenticated, useQuery } from "convex/react";
 
 export default function Page() {
   const users = useQuery(api.user.getMany);
 
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <p>apps/web</p>
-      {JSON.stringify(users)}
-    </div>
+    <>
+      <Authenticated>
+        <div className="flex items-center justify-center min-h-svh">
+          <p>apps/web</p>
+          <UserButton />
+          {JSON.stringify(users)}
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton>Sign in</SignInButton>
+      </Unauthenticated>
+    </>
   );
 }
