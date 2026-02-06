9d0794468b58fecd74546a587c2b96d46dc8e0f8
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2026-02-06 22:19:50 +0700

    AI Voice Assistant

---
+++ apps/widget/modules/widget/hooks/use-vapi.ts
@@ -0,0 +1,86 @@
+import Vapi from "@vapi-ai/web";
+import { useEffect, useState } from "react";
+
+interface TranscriptMessage {
+  role: "user" | "assistant";
+  text: string;
+}
+
+export const useVapi = () => {
+  const [vapi, setVapi] = useState<Vapi | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [isConnecting, setIsConnecting] = useState(false);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [transcript, setTranscript] = useState<TranscriptMessage[]>([]);
+
+  useEffect(() => {
+    // Chỉ để test , người dùng phải cho api của họ
+    const vapiInstance = new Vapi("6b0d6084-8d7d-4f9c-83f4-4ca5ef8b1e04");
+    setVapi(vapiInstance);
+
+    vapiInstance.on("call-start", () => {
+      setIsConnected(true);
+      setIsConnecting(false);
+      setTranscript([]);
+    });
+
+    vapiInstance.on("call-end", () => {
+      setIsConnected(false);
+      setIsConnecting(false);
+      setIsSpeaking(false);
+    });
+
+    vapiInstance.on("speech-start", () => {
+      setIsSpeaking(true);
+    });
+
+    vapiInstance.on("speech-end", () => {
+      setIsSpeaking(false);
+    });
+
+    vapiInstance.on("error", (error) => {
+      console.log("Error : ", error);
+      setIsConnecting(false);
+    });
+
+    vapiInstance.on("message", (message) => {
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        setTranscript((prev) => [
+          ...prev,
+          {
+            role: message.role === "user" ? "user" : "assistant",
+            text: message.transcript,
+          },
+        ]);
+      }
+    });
+
+    return () => {
+      vapiInstance.stop();
+    };
+  }, []);
+
+  const startCall = () => {
+    setIsConnecting(true);
+
+    if (vapi) {
+      // Chỉ dành để test
+      vapi.start("d9e9efdc-b73c-48b7-8bac-eb12821b08f3");
+    }
+  };
+
+  const endCall = () => {
+    if (vapi) {
+      vapi.stop();
+    }
+  };
+
+  return {
+    isConnected,
+    isConnecting,
+    isSpeaking,
+    transcript,
+    startCall,
+    endCall,
+  };
+};
