3e7ebfeef7257bc68106c01ebed5c37c410e2ada
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2026-02-06 12:02:30 +0700

    Organizations

---
+++ apps/web/app/(dashboard)/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { api } from "@workspace/backend/_generated/api";
+import { useQuery } from "convex/react";
+
+export default function Page() {
+  const users = useQuery(api.user.getMany);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>apps/web</p>
+      <UserButton />
+      <OrganizationSwitcher hidePersonal />
+      {JSON.stringify(users)}
+    </div>
+  );
+}
