48b7a90a22deeba7f7c56829ac3f08aa12a342ab
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2026-02-07 14:58:17 +0700

    Widget Screen Router

---
+++ apps/widget/modules/widget/atoms/widget-atom.tsx
@@ -0,0 +1,4 @@
+import { WidgetScreen } from "@/modules/widget/type";
+import { atom } from "jotai";
+
+export const screenAtom = atom<WidgetScreen>("auth");
--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,17 +1,30 @@
 "use client";
 
-import { WidgetFooter } from "../components/widget-footer";
-import { WidgetAuthScreen } from "../screens/widget-auth-screen";
+import { screenAtom } from "@/modules/widget/atoms/widget-atom";
+import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen";
+import { useAtomValue } from "jotai";
 
 interface Props {
   organizationId: string;
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom);
+
+  const screenComponents = {
+    error: <p>Error</p>,
+    loading: <p>Loading</p>,
+    auth: <WidgetAuthScreen />,
+    voice: <p>Voice</p>,
+    inbox: <p>Inbox</p>,
+    selection: <p>Selection</p>,
+    chat: <p>Chat</p>,
+    contact: <p>Contact</p>,
+  };
+
   return (
     <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
-      <WidgetFooter />
+      {screenComponents[screen]}
     </main>
   );
 };
